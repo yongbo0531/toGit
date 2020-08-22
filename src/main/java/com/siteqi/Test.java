a7ee89e2f61a20122341230fb6b0b7909db58c5e
zhangyongbo <zhangyb_oup@si-tech.com.cn>
2020-08-22 09:38:15 +0800

    git提交到本地库

---
+++ src/main/java/com/siteqi/Test.java
@@ -0,0 +1,11 @@
+package com.siteqi;
+
+/**
+ * 永波
+ * 2020/8/22
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("git======================");
+    }
+}
