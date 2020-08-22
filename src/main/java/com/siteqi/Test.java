c20718f53c79e99ced8edf3666f7041ce7228686
zhangyongbo <zhangyb_oup@si-tech.com.cn>
2020-08-22 10:02:15 +0800

    增加bbbbbbbb

--- src/main/java/com/siteqi/Test.java
+++ src/main/java/com/siteqi/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("git======================");
         System.out.println("aaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("bbbbbbbbbbbbbbbbbbbb");
     }
 }
